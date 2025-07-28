acf693263d44eb25071cb537061bb444892a5380
Manu Gurudath <manusvathgurudath@gmail.com>
2025-07-28 18:45:40 +0000

    upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
